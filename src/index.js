aad92ea0fe998a80ae8ed4cb0184ece831cfda76
Roberto Maia <romaiajr5@gmail.com>
2020-12-05 14:16:41 -0300

    Ready for test

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("./database/config");
+const mongoose = require("mongoose");
+var cors = require("cors");
+
+const app = express();
+/**
+ *Database Setup
+ */
+mongoose
+  .connect(process.env.MONGO_URL || db.uri, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
+app.listen(3001, () =>
+  console.log(`Sua API REST está funcionando na porta 3001 `)
+);
+
+module.exports = new App().express;
