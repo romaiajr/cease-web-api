f691c8a8d666a149c5d0a185c8298e82655c1aba
Roberto Maia <romaiajr5@gmail.com>
2020-12-06 22:20:44 -0300

    Removendo logs desnecessários

--- src/app/controller/FotoController.js
+++ src/app/controller/FotoController.js
@@ -2,13 +2,11 @@ const Foto = require("../model/Foto");
 
 class FotoController {
   async store(req, res) {
-    console.log(req.body);
     const data = await Foto.create(req.body);
     return res.json(data);
   }
   async index(req, res) {
     const data = await Foto.find({ idEvento: req.body.data });
-    console.log(data);
     return res.json(data);
   }
   async remove(req, res) {
